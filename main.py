3b6acd6eb7f6b96558624bdeaa4e5a87bbfff31d
kailash-unc <kailash@email.unc.edu>
2024-05-07 19:54:54 -0400

    Working discord export functionality

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from api import chats
+app = FastAPI()
+
+@app.get("/")
+def healthCheck():
+    print("reach")
+    return {"status": "Application Health - Green"}
+
+
+app.include_router(chats.api)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
